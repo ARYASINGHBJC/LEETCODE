378323b6bed699bdc475fc1413367d8d69a64c6d
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-10-30 22:08:32 +0530

    Time: 34 ms (93.19%), Space: 13.8 MB (80.70%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,14 +1,26 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':return 0
-        dp = [0]*(len(s)+1)
-        dp[0:2] = [1,1]
-        for i in range(2, len(s)+1):
-            if int(s[i-1:i]) != 0:
-                dp[i] = dp[i-1]
-            if 10 <= int(s[i-2:i]) <= 26:
-                dp[i] += dp[i-2]
-        return dp[-1]
+        # ------------------- Bottom Up -------------
+        
+        n = len(s)
+        dp = [0]*(n+1)
+        dp[n] = 1
+        for i in range(n-1, -1, -1):
+            if s[i] != '0':
+                dp[i] = dp[i+1]
+            if i+1 < n and '10' <= s[i:i+2] <= '26':
+                dp[i] += dp[i+2]
+        return dp[0]
+        # ---------------- Top Down -----------------
+        # if not s or s[0] == '0':return 0
+        # dp = [0]*(len(s)+1)
+        # dp[0:2] = [1,1]
+        # for i in range(2, len(s)+1):
+        #     if int(s[i-1:i]) != 0:
+        #         dp[i] = dp[i-1]
+        #     if 10 <= int(s[i-2:i]) <= 26:
+        #         dp[i] += dp[i-2]
+        # return dp[-1]
         # dp = [0]*len(s)
         # if not s or s[0] == '0':
         #     return 0
