63c2e4e49e0b235aa22ae7ca3d8feb41b45f1fa6
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-10-30 23:27:09 +0530

    Time: 56 ms (64.14%), Space: 13.9 MB (80.70%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,16 +1,32 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # ------------------- Bottom Up -------------
-        
-        n = len(s)
-        dp = [0]*(n+1)
-        dp[n] = 1
-        for i in range(n-1, -1, -1):
+        # ------------------Space Optimised----------
+        if s[0] == '0': return 0
+        prev_1, prev_2 = 1, 1
+        for i in range(1, len(s)):
+            curr = 0
+			# if current index is number in 1->9 range, we can make dp[i] = dp[i-1]
             if s[i] != '0':
-                dp[i] = dp[i+1]
-            if i+1 < n and '10' <= s[i:i+2] <= '26':
-                dp[i] += dp[i+2]
-        return dp[0]
+                curr += prev_2
+			# if current index and the prev index make a number in 0 -> 26 range, we can add dp[i-2]
+            if 10 <= int(s[i-1:i+1]) <= 26:
+                if i > 1:
+                    curr += prev_1
+                else:
+                    curr +=1
+            prev_1 = prev_2
+            prev_2 = curr
+        return prev_2   
+        # ------------------- Bottom Up -------------
+        # n = len(s)
+        # dp = [0]*(n+1)
+        # dp[n] = 1
+        # for i in range(n-1, -1, -1):
+        #     if s[i] != '0':
+        #         dp[i] = dp[i+1]
+        #     if i+1 < n and '10' <= s[i:i+2] <= '26':
+        #         dp[i] += dp[i+2]
+        # return dp[0]
         # ---------------- Top Down -----------------
         # if not s or s[0] == '0':return 0
         # dp = [0]*(len(s)+1)
