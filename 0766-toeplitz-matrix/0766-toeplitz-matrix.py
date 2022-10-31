9531867d10886fd0bb427302bb9ae978aca52186
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-10-31 19:29:59 +0530

    Time: 117 ms (79.45%), Space: 13.9 MB (36.74%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -3,7 +3,11 @@ class Solution:
         row = len(matrix)
         col = len(matrix[0])
         for i in range(row - 1):
-            for j in range(col - 1):
-                if matrix[i][j] != matrix[i+1][j+1]:
-                    return False
+            if matrix[i][:-1] != matrix[i+1][1:]:
+                return False
         return True
+        # for i in range(row - 1):
+        #     for j in range(col - 1):
+        #         if matrix[i][j] != matrix[i+1][j+1]:
+        #             return False
+        # return True
