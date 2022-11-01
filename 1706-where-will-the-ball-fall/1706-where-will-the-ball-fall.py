cd6dd72f27ef48fd63ffc40763a906bd94e9d391
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-11-01 13:40:52 +0530

    Time: 252 ms (81.42%), Space: 14.3 MB (55.41%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -1,16 +1,26 @@
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
-        ans = [-1]*len(grid[0])
         colLength = len(grid[0])
         rowLength = len(grid)
-        for col in range(colLength):
-            currentCol = col
+        def validate(col):
             for row in range(rowLength):
-                nextColumn = currentCol + grid[row][currentCol]
-                if nextColumn < 0 or nextColumn > colLength - 1 or grid[row][currentCol] != grid[row][nextColumn]:
-                    ans[col] = -1
-                    break
-                ans[col] = nextColumn
-                currentCol = nextColumn
-        return ans
+                if col < colLength-1 and  grid[row][col] + grid[row][col+1] == 2: col += 1 #1, 1
+                elif  col > 0 and  grid[row][col] + grid[row][col-1] == -2: col -= 1 #-1, -1
+                else: return -1
+            return col
+        
+        return [validate(i) for i in range(colLength)]
+        # ans = [0]*len(grid[0])
+        # colLength = len(grid[0])
+        # rowLength = len(grid)
+        # for col in range(colLength):
+        #     currentCol = col
+        #     for row in range(rowLength):
+        #         nextColumn = currentCol + grid[row][currentCol]
+        #         if nextColumn < 0 or nextColumn > colLength - 1 or grid[row][currentCol] != grid[row][nextColumn]:
+        #             ans[col] = -1
+        #             break
+        #         ans[col] = nextColumn
+        #         currentCol = nextColumn
+        # return ans
                         
