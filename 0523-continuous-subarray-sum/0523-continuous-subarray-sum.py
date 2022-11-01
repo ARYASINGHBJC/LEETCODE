2c7a3f071c352946c1bff6263c8123604744bf75
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-11-01 13:49:28 +0530

    Time: 1078 ms (88.23%), Space: 33.2 MB (69.92%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,19 +1,11 @@
-class Solution(object):
-    def checkSubarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-        # initialize the hash map with index 0 for sum 0
-        hash_map = {0: 0}
-        s = 0
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        numMap = {0:0}
+        sm = 0
         for i in range(len(nums)):
-            s += nums[i]
-            # if the remainder s % k occurs for the first time
-            if s % k not in hash_map:
-                hash_map[s % k] = i + 1
-            # if the subarray size is at least two
-            elif hash_map[s % k] < i:
+            sm += nums[i]
+            if sm % k not in numMap:
+                numMap[sm%k] = i + 1
+            if numMap[sm % k] < i:
                 return True
         return False
