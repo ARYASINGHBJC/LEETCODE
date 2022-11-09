46865dfea91fba4c9999cadcaa58ebdc5181dab5
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-11-09 11:53:05 +0530

    Time: 1052 ms (64.64%), Space: 48.9 MB (90.23%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -6,12 +6,24 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-        if root.left and not root.right:
-            return 1 + self.minDepth(root.left)
-        if root.right and not root.left:
-            return 1 + self.minDepth(root.right)
-        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+        # if not root:
+        #     return 0
+        # if not root.left and not root.right:
+        #     return 1
+        # if root.left and not root.right:
+        #     return 1 + self.minDepth(root.left)
+        # if root.right and not root.left:
+        #     return 1 + self.minDepth(root.right)
+        # return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+        # --------------bfs---------------
+        if not root: return 0
+        q = deque([root])
+        level = 1
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if not node.left and not node.right: return level
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            level +=1
+        return level
