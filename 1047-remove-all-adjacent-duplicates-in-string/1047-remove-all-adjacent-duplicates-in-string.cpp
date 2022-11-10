0924374840767e9d0aa581c7bd0a53ed97f3195a
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-11-10 10:26:10 +0530

    Time: 10 ms (99.40%), Space: 10.4 MB (81.99%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i = 0;
+        for(int j =0; j < s.size(); ++j, ++i){
+            s[i] = s[j];
+            if(i > 0 && s[i-1] == s[i]){
+                i -= 2;
+            }
+        }
+        return s.substr(0, i);
+    }
+};
