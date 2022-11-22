60114f3f2b28195d9fbe7e1767f13eb4c8c126da
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-11-22 20:30:09 +0530

    Time: 42 ms (98.88%), Space: 13.8 MB (99.52%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        while( n % 4 == 0 ):
+            # Reduction by factor of 4
+            n //= 4
+            
+        if n % 8 == 7:
+            # Quick response for n = 8k + 7
+            return 4
+        
+        # Check whether n = a^2 + b^2
+        for a in range( int(sqrt(n))+1 ):
+            
+            b = int( sqrt( n - a*a ) )
+            
+            if ( a**2 + b ** 2 ) == n :
+                return (a>0) + (b>0)
+            
+        # n = a^2 + b^2 + c^2
+        return 3
+        # dp = [i for i in range(n+1)]
+        # for i in range(2,n+1):
+        #     for j in range(1,int(i ** 0.5) + 1):
+        #         dp[i] = min(dp[i],dp[i-j*j]+1)
+        # return dp[n]
