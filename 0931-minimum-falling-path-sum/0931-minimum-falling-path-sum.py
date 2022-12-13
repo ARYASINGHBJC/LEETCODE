690b73e72d1273564f9a30ea9dc4827d0b48594d
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2022-12-13 16:55:43 +0530

    Time: 352 ms (39.76%), Space: 14.6 MB (91.12%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        A = matrix
+        dp = A[0]
+        for row in A[1:]:
+            dp = [value + min([dp[c], dp[max(c - 1, 0)], dp[min(len(A) - 1, c + 1)]]) for c, value in enumerate(row)]
+        return min(dp)
