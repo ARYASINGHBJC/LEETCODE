e4cfa20171479104ccee4f0b41d2f8897caef886
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2023-01-01 16:38:34 +0530

    Time: 36 ms (73.96%), Space: 13.8 MB (99.08%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,23 +1,46 @@
-class Solution:
-    def wordPattern(self, pattern: str, str: str) -> bool:
-        # dictionary approach 
-        # Time complexity: O(n)
-        # Space complexity: O(n)
+# class Solution:
+#     def wordPattern(self, pattern: str, str: str) -> bool:
+#         # dictionary approach 
+#         # Time complexity: O(n)
+#         # Space complexity: O(n)
         
-        words = str.split(" ")
-        if not len(words) == len(pattern):
-            return False
+#         words = str.split(" ")
+#         if not len(words) == len(pattern):
+#             return False
         
-        mapping = dict() # key is the pattern, value is the word
+#         mapping = dict() # key is the pattern, value is the word
         
-        for i in range(len(words)):
-            if pattern[i] not in mapping:
-                # values() is a set - fast membership testing - O(1) amortised search
-                if words[i] not in mapping.values(): 
-                    mapping[pattern[i]] = words[i]
-                else:
+#         for i in range(len(words)):
+#             if pattern[i] not in mapping:
+#                 # values() is a set - fast membership testing - O(1) amortised search
+#                 if words[i] not in mapping.values(): 
+#                     mapping[pattern[i]] = words[i]
+#                 else:
+#                     return False
+#             else:
+#                 if not mapping[pattern[i]] == words[i]:
+#                     return False
+#         return True
+class Solution():
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        words_list = s.split(" ")
+        if len(pattern) != len(words_list):
+            return False 
+        
+        pattern_map = {}
+        for p, word in zip(pattern, words_list):
+            if p not in pattern_map:
+                if word in pattern_map.values():
                     return False
+                pattern_map[p] = word 
             else:
-                if not mapping[pattern[i]] == words[i]:
+                if pattern_map[p] != word:
                     return False
+                
+                
         return True
