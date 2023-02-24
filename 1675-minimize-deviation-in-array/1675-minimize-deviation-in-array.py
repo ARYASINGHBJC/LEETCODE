c6b63039483a947371c7c7388f73b6e09363bb5d
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2023-02-24 23:03:37 +0530

    Time: 2320 ms (61.19%), Space: 21.5 MB (86.57%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumDeviation(self, A):
+        pq = []
+        for a in A:
+            heapq.heappush(pq, -a * 2 if a % 2 else -a)
+        res = float('inf')
+        mi = -max(pq)
+        while len(pq) == len(A):
+            a = -heapq.heappop(pq)
+            res = min(res, a - mi)
+            if a % 2 == 0:
+                mi = min(mi, a // 2)
+                heapq.heappush(pq, -a // 2)
+        return res
