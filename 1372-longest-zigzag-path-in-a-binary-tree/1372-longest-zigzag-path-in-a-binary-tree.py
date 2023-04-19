b9a6cc82fcd680f950703b2eaec9a2ddea142b05
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2023-04-19 09:57:09 +0530

    Time: 504 ms (28.57%), Space: 26.2 MB (100.00%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
@@ -4,21 +4,32 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+# class Solution:
 
-    def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        self.pathLength = 0
+#     def longestZigZag(self, root: Optional[TreeNode]) -> int:
+#         self.pathLength = 0
         
-        def dfs(node, goLeft, steps):
+#         def dfs(node, goLeft, steps):
+#             if node:
+#                 self.pathLength = max(self.pathLength, steps)
+#                 if goLeft:
+#                     dfs(node.left, False, steps + 1)
+#                     dfs(node.right, True, 1)
+#                 else:
+#                     dfs(node.left, False, 1)
+#                     dfs(node.right, True, steps + 1)
+        
+#         dfs(root, False, 0)
+#         dfs(root, True, 0)
+#         return self.pathLength
+class Solution:
+    def longestZigZag(self, root: TreeNode) -> int:
+        ans = 0
+        nodes = [(root, False, 0), (root, True, 0)]
+        while nodes:
+            node, right, length = nodes.pop()
             if node:
-                self.pathLength = max(self.pathLength, steps)
-                if goLeft:
-                    dfs(node.left, False, steps + 1)
-                    dfs(node.right, True, 1)
-                else:
-                    dfs(node.left, False, 1)
-                    dfs(node.right, True, steps + 1)
-        
-        dfs(root, False, 0)
-        dfs(root, True, 0)
-        return self.pathLength
+                ans = max(ans, length)
+                nodes.append((node.right if right else node.left, not right, length + 1))
+                nodes.append((node.left if right else node.right, right, 1))
+        return ans
