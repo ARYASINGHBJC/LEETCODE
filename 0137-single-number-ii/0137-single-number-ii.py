89f6e215187361638a72db5188ce4351096c6096
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2023-07-04 18:34:31 +0530

    Time: 79 ms (50.61%), Space: 18.2 MB (61.98%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,11 +1,21 @@
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         d = {}
+#         for ele in nums:
+#             if ele not in d:
+#                 d[ele] = 1
+#             else:
+#                 d[ele] += 1
+#         for key,val in d.items():
+#             if val == 1:
+#                 return key
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        d = {}
-        for ele in nums:
-            if ele not in d:
-                d[ele] = 1
-            else:
-                d[ele] += 1
-        for key,val in d.items():
-            if val == 1:
-                return key
+  def singleNumber(self, nums: List[int]) -> int:
+    ones = 0
+    twos = 0
+
+    for num in nums:
+      ones ^= (num & ~twos)
+      twos ^= (num & ~ones)
+
+    return ones
