3efe2903aa6e86a385f6c8584f505277a7e14942
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2025-01-25 21:22:47 +0530

    Time: 11 ms (48.39%), Space: 18.6 MB (40.31%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,19 +1,22 @@
-
 class Solution:
+    # def helper(self, idx, jumps, nums):
+    #     if idx >= len(nums) -1:
+    #         return jumps
+    #     if nums[idx] == 0:
+    #         return float('inf')
+    #     mn = float('inf')
+    #     for i in range(1, nums[idx] + 1):
+    #         mn = min(mn, self.helper(idx+i, jumps+1, nums))
+    #     return mn
     def jump(self, nums: List[int]) -> int:
-        # The starting range of the first jump is [0, 0]
-        answer, n = 0, len(nums)
-        cur_end, cur_far = 0, 0
+        ans,n = 0, len(nums)
+        curr_end, curr_far = 0, 0
+        for i in range(n-1):
+            curr_far = max(curr_far, i+nums[i])
+            if i == curr_end:
+                ans += 1
+                curr_end = curr_far
+        return ans  
+        # return self.helper(0,0, nums)
         
-        for i in range(n - 1):
-            # Update the farthest reachable index of this jump.
-            cur_far = max(cur_far, i + nums[i])
-
-            # If we finish the starting range of this jump,
-            # Move on to the starting range of the next jump.
-            if i == cur_end:
-                answer += 1
-                cur_end = cur_far
-                
-        return answer
         
