6caf53b5a65342614848a4fe27782b1a1441468f
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2025-02-18 01:09:17 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,20 +1,12 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        next_greater = {}
-        # Create a stack to keep track of the elements whose next greater element is not found yet
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        seen = {}
         stack = []
         for num in nums2:
-            # While the stack is not empty and the current number is greater than the top element in the stack
-            while stack and num > stack[-1]:
-                # Pop the top element from the stack and set its next greater element to the current number
-                next_greater[stack.pop()] = num
-            # Add the current number to the stack
+            while stack and stack[-1] < num:
+                seen[stack.pop()] = num
             stack.append(num)
-        result = []
-        for num in nums1:
-            # If the next greater element of the current number is found
-            if num in next_greater:
-                result.append(next_greater[num])
-            else:
-                result.append(-1)
-        return result
+        for ele in stack:
+            seen[ele] = -1
+        return [seen[num] for num in nums1]
+        
