6a514e5a7133d09f68cb17eab626bcd350cd1550
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2025-05-04 16:58:44 +0530

    Time: 1 ms (99.78%), Space: 43.3 MB (68.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,35 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {        
-        if(s.length() != t.length()){
+    public boolean isAnagram(String s, String t) {     
+        int temp[] = new int[26];
+        if(s.length()!= t.length()){
             return false;
-        }
-        HashMap<Character, Integer> seen = new HashMap<>();
-        for(int i = 0; i < s.length();i++){
-            char currChar = s.charAt(i);
-            seen.put(currChar, seen.getOrDefault(currChar, 0) + 1);
-        }
-        for(int i = 0; i<t.length();i++){
-            char currChar = t.charAt(i);
-            if(!seen.containsKey(currChar) || seen.get(currChar) == 0){
+        } 
+        for (char cr: s.toCharArray()){
+            temp[cr - 'a']++;
+        }   
+        for(char cr: t.toCharArray()){
+            if(temp[cr - 'a'] == 0){
                 return false;
-            }
-            else{
-                seen.put(currChar, seen.get(currChar) - 1);
-            }
+            }temp[cr - 'a']--;
         }
         return true;
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // HashMap<Character, Integer> seen = new HashMap<>();
+        // for(int i = 0; i < s.length();i++){
+        //     char currChar = s.charAt(i);
+        //     seen.put(currChar, seen.getOrDefault(currChar, 0) + 1);
+        // }
+        // for(int i = 0; i<t.length();i++){
+        //     char currChar = t.charAt(i);
+        //     if(!seen.containsKey(currChar) || seen.get(currChar) == 0){
+        //         return false;
+        //     }
+        //     else{
+        //         seen.put(currChar, seen.get(currChar) - 1);
+        //     }
+        // }
+        // return true;
     }
 }
