282cc7a7904be378d5e15e67c77598638d0af270
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2025-05-04 16:51:10 +0530

    Time: 18 ms (16.65%), Space: 44.9 MB (26.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {        
-        if (s.length() != t.length()) {
-            return false; // Anagrams must have the same length
+        if(s.length() != t.length()){
+            return false;
         }
-
-        HashMap<Character,Integer> seen = new HashMap<>();
-        for(int i = 0; i<s.length();i++){
+        HashMap<Character, Integer> seen = new HashMap<>();
+        for(int i = 0; i < s.length();i++){
             char currChar = s.charAt(i);
-            seen.put(currChar, seen.getOrDefault(currChar, 0) +1);
+            seen.put(currChar, seen.getOrDefault(currChar, 0) + 1);
         }
         for(int i = 0; i<t.length();i++){
-             char currChar = t.charAt(i);
-            if(!seen.containsKey(currChar) || seen.get(currChar) ==0){
+            char currChar = t.charAt(i);
+            if(!seen.containsKey(currChar) || seen.get(currChar) == 0){
                 return false;
-            }else{
-                seen.put(currChar, seen.get(currChar) -1);
+            }
+            else{
+                seen.put(currChar, seen.get(currChar) - 1);
             }
         }
         return true;
