e9ce6f62d35bbe2e7d0c56158ea6a8c86d9439d2
Arya Singh <72375247+ARYASINGHBJC@users.noreply.github.com>
2025-05-04 12:16:49 +0530

    Time: 0 ms (100%), Space: 41.5 MB (91.01%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,20 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         s = s.trim();
+//         String[] words = s.split(" +");
+//         return words[words.length - 1].length();
+//     }
+// }
+
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        String[] words = s.split(" +");
-        return words[words.length - 1].length();
+        int count = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i) == ' '){
+                if(count != 0){return count;}
+            }
+            else { count++;}
+        }
+        return count;
     }
 }
